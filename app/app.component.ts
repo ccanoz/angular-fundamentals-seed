f718213d3dceafa7747619be6785078d41a9150a
Cyra <cyraa@live.ca>
2021-12-12 14:53:04 -0800

    class and style binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,12 @@
 // Component decorator
 import { Component } from '@angular/core';
 
+interface Passenger {
+  id: number,
+  fullname: string,
+  checkedIn: boolean
+}
+
 @Component({
   // select = element in html <app-root>
   selector: 'app-root',
@@ -9,19 +15,52 @@ import { Component } from '@angular/core';
   // alternatively can use templateUrl
   template: `
     <div class="app">
-      <button (click)="handleClick(username.value)">
-        Get value
-      </button>
-      <input type="text" #username>
-      <div>{{ name }}</div>
+      <h3>Airline Passengers</h3>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i = index;">
+          <span class="status"
+          [class.checked-in]="passenger.checkedIn"></span>
+           {{ i }} : {{ passenger.fullname }}
+        </li>
+      </ul>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i = index;">
+          <span class="status"
+                [ngClass]="{
+                    'checked-in': passenger.checkedIn,
+                    'checked-out': !passenger.checkedIn}">
+          </span>
+           {{ i }} : {{ passenger.fullname }}
+        </li>
+      </ul>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i = index;">
+          <span class="status"
+                [style.backgroundColor]="(passenger.checkedIn ? '#2ecc71' : '#c0392b')">
+          </span>
+           {{ i }} : {{ passenger.fullname }}
+        </li>
+      </ul>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i = index;">
+          <span class="status"
+                [ngStyle]="{'backgroundColor':(passenger.checkedIn ? '#2ecc71' : '#c0392b')}">
+          </span>
+           {{ i }} : {{ passenger.fullname }}
+        </li>
+      </ul>
     </div>
   `
 })
 export class AppComponent {
-  name: string = 'Cyra';
-
-  handleClick(value: string) {
-    console.log(value);
-  }
+  passengers: Passenger[] = [{
+    id: 1,
+    fullname: "Stephen",
+    checkedIn: true
+  }, {
+    id: 2,
+    fullname: "Rose",
+    checkedIn: false
+  }]
 }
 
